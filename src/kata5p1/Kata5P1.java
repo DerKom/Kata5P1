7ec5fd426b647a29a4cf648399e917654a2164e2
DerKom <mac_alicante@hotmail.com>
2018-01-19 21:05:32 +0000

    Version 3 de la Kata 5 ;))

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,5 +1,10 @@
 package kata5p1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,7 +13,7 @@ import java.sql.Statement;
 
 public class Kata5P1 {
 
-     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
         Connection connection = DriverManager.getConnection("jdbc:sqlite:kata5.db");
         
@@ -18,11 +23,20 @@ public class Kata5P1 {
         while (rs.next()) {
             System.out.println("ID = " + rs.getInt("Id"));
             System.out.println("NAME = " + rs.getString("Name"));
-            System.out.println("APELLIDOS = " + rs.getString("Apellidos"));
-            System.out.println("ORGANIZACION = " + rs.getString("Departamento"));
         }
         
-        statement.execute("CREATE TABLE 'MAIL' ('Id' INTEGER PRIMARY KEY AUTOINCREMENT," + "'Mail' INTEGER NOT NULL);");
+        statement.execute("CREATE TABLE IF NOT EXISTS 'MAIL' (" + "'Id' INTEGER PRIMARY KEY AUTOINCREMENT," + "'Mail' INTEGER NOT NULL);");
+        
+        String nameFile = "emails.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(new File(nameFile)));
+        String mail;
+        String query;
+        
+        while ((mail = reader.readLine()) != null) {
+            if (!mail.contains("@")) continue;
+            query = "INSERT INTO MAIL (Mail) VALUES ('" + mail + "');";
+            statement.executeUpdate(query);
+        }
     }
-     
+    
 }
